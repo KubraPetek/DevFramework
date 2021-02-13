3297361637b86edb64683ce06ff758bbab03602a
Kübra PETEK <kubrapetekk@gmail.com>
2021-02-13 23:03:42 +0300

    second commit

---
+++ DevFramewok.NorthWind.Business/Concreate/Managers/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using DevFramewok.NorthWind.Business.Abstract;
+using DevFramework.NorthWind.DataAccess.Abstract;
+using DevFramework.NorthWind.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramewok.NorthWind.Business.Concreate.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+
+        }
+    }
+}
