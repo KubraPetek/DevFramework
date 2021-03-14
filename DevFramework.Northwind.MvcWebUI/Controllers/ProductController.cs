655183b63e23e68f35213eee2993d712051f7bbf
Kübra PETEK <kubrapetekk@gmail.com>
2021-03-14 20:12:02 +0300

    MVC Framework Entegrasyonu

---
+++ DevFramework.Northwind.MvcWebUI/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using DevFramewok.NorthWind.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFramework.Northwind.MvcWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public ActionResult Index()
+        {
+            //return View(_productService.GetAll()); --bunu asla yapma//viewback ve viewData kullamak zorunda kalabiliriz
+
+            var model = new ProductListViewModel
+            {
+                Products=_productService.GetAll()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ DevFramework.Northwind.MvcWebUI/Models/ProductListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using DevFramework.NorthWind.Entities.Concreate;
+
+namespace DevFramework.Northwind.MvcWebUI
+{
+    public class ProductListViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
