910fa4af1604ce2844fb59dd662107ab6bfa8dc8
Kübra PETEK <kubrapetekk@gmail.com>
2021-04-10 15:56:12 +0300

    Channel Factory İmplementasyonu

---
+++ DevFramewok.NorthWind.Business/DependencyResolvers/Ninject/ServiceModule.cs
@@ -0,0 +1,20 @@
+﻿using DevFramewok.NorthWind.Business.Abstract;
+using DevFramework.Core.Utulities.Common;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramewok.NorthWind.Business.DependencyResolvers.Ninject
+{
+   public  class ServiceModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().ToConstant(WcfProxy<IProductService>.CreateChannel());
+
+        }
+    }
+}
---
+++ DevFramework.Core/Utulities/Common/WcfProxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.Utulities.Common
+{
+    public class WcfProxy<T>
+    {
+        //servise servis contract üzerinden erişilir 
+        //Burda T olarak gönderdiğimiz tip mesela IProductService olabilir
+        public static T CreateChannel()
+        {
+            // servisin abc sinin oluşturulduğu yer
+            string baseAddress = ConfigurationManager.AppSettings["ServiceAddress"];
+            string address = string.Format(baseAddress, typeof(T).Name.Substring(1));
+            var binding = new BasicHttpBinding();
+            var channel = new ChannelFactory<T>(binding,address);
+
+            return channel.CreateChannel();
+        }
+    }
+}
